6c883ea36beaf0020c821d7363b0bfc09df56d6e
wektor1 <wektor1@gmail.com>
2018-08-16 11:23:08 +0200

    Refactoring

--- src/bracketsVerification.cpp
+++ src/bracketsVerification.cpp
@@ -1,28 +1,34 @@
 #include "../inc/bracketsVerification.hpp"
 #include <stack>
 
+bool popOperation(std::stack<char> &_stack, char correct_prev_bracket) {
+  if (!_stack.empty() && _stack.top() == correct_prev_bracket) {
+    _stack.pop();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void charReader(char bracket, std::stack<char> &_stack, bool &is_balanced) {
+  switch (bracket) {
+  case '}':
+    is_balanced = popOperation(_stack, '{');
+    break;
+  case ')':
+    is_balanced = popOperation(_stack, '(');
+    break;
+  default:
+    _stack.push(bracket);
+    break;
+  }
+}
+
 bool bracketsVerification(std::string input) {
   std::stack<char> char_stack;
   bool is_balanced = true;
   for (auto bracket : input) {
-    switch (bracket) {
-    case '}':
-      if (!char_stack.empty() && char_stack.top() == '{')
-        char_stack.pop();
-      else
-        is_balanced = false;
-      break;
-
-    case ')':
-      if (!char_stack.empty() && char_stack.top() == '(')
-        char_stack.pop();
-      else
-        is_balanced = false;
-      break;
-    default:
-      char_stack.push(bracket);
-      break;
-    }
+    charReader(bracket, char_stack, is_balanced);
   }
   if (!char_stack.empty()) {
     is_balanced = false;

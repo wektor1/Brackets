80ed717dae930686b967d04b1805e6b1cd686aa9
wektor1 <wektor1@gmail.com>
2018-08-19 00:03:43 +0200

    more tests

--- test_gtest/bracketsVerification-Tests.cpp
+++ test_gtest/bracketsVerification-Tests.cpp
@@ -6,8 +6,10 @@ TEST(bracketsVerification, balanced) {
   EXPECT_TRUE(bracketsVerification("{}"));
   EXPECT_TRUE(bracketsVerification("{}()"));
   EXPECT_TRUE(bracketsVerification("{()}"));
+  EXPECT_TRUE(bracketsVerification("{(){}[]}"));
 }
 
 TEST(bracketsVerification, inbalanced) {
   EXPECT_FALSE(bracketsVerification("{"));
+  EXPECT_FALSE(bracketsVerification("{[(]})"));
 }
